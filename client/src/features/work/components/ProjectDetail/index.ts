60f4ee622793f2098bca846a374dac8fde21759d
Nicolas Santos <nicolassoares.santos@outlook.com>
2026-04-12 21:31:44 -0300

    refactor: rename BlockRender.tsx to BlockRenderer.tsx

--- client/src/features/work/components/ProjectDetail/index.ts
+++ client/src/features/work/components/ProjectDetail/index.ts
@@ -1,5 +1,5 @@
 // Barrel exports for project-detail components
-export { BlockRenderer } from './BlockRenderer/BlockRender';
+export { BlockRenderer } from './BlockRenderer/BlockRenderer';
 export { ContentLayout } from './ContentLayout/ContentLayout';
 export { SummaryNav } from './SummaryNav/SummaryNav';
 export { default as ProjectDetailHero } from './ProjectDetailHero/ProjectDetailHero';
